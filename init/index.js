1caa3165693570c1e1273b104526474dc07e4bbf
Deepak4414 <dkvu4414@gmail.com>
2024-11-16 14:33:00 +0530

    1 commit

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({...obj, owner:'67382d3fae89dc4b7b6c30cf'}));     // owner id is keep in single kote ' 
+
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
